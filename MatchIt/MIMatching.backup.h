d90b77a9b6b25630931d99989bb746fffc770f31
hxy060799 <34531212@qq.com>
2013-02-02 11:32:56 +0800

    小的修改

---
+++ MatchIt/MIMatching.backup.h
@@ -0,0 +1,31 @@
+//
+//  MIMatching.h
+//  MatchIt
+//
+//  Created by Bill on 13-1-12.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "MIPositionConvert.h"
+#import "MIBlockManager.h"
+#import "MIConfig.h"
+
+@interface MIMatching : NSObject
+
++(BOOL)isMatchingAWithA:(struct MIPosition)blockA B:(struct MIPosition)blockB Manager:(MIBlockManager *)manager;
+
++(BOOL)isMatchingBWithA:(struct MIPosition)blockA B:(struct MIPosition)blockB Manager:(MIBlockManager*)manager;
++(BOOL)isMatchingBWithA:(struct MIPosition)blockA B:(struct MIPosition)blockB Manager:(MIBlockManager *)manager HorizontalFlip:(BOOL)flip;
+
++(BOOL)isMatchingCWithA:(struct MIPosition)blockA B:(struct MIPosition)blockB Manager:(MIBlockManager*)manager;
++(BOOL)isMatchingCWithA:(struct MIPosition)blockA B:(struct MIPosition)blockB Manager:(MIBlockManager *)manager HorizontalFlip:(BOOL)flip;
+
++(BOOL)isMatchingDWithA:(struct MIPosition)blockA B:(struct MIPosition)blockB Manager:(MIBlockManager*)manager;
++(BOOL)isMatchingDWithA:(struct MIPosition)blockA B:(struct MIPosition)blockB Manager:(MIBlockManager *)manager HorizontalFlip:(BOOL)flip;
+
+//在算法部分没有完成之前,用这个方法把算法走过的方块标记出来,用来调试算法
++(void)markBlockWithNumber:(int)number position:(struct MIPosition)position Manager:(MIBlockManager*)manager HorizontalFlip:(BOOL)flip;
+
+@end
